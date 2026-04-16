4cca4682e6cee78df0f64db8fba4427b53341b94
Sisir Paudel <sisir8848@gmail.com>
2026-04-17 00:26:01 +0545

    Delete server directory

--- server/src/modules/users/admin.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-import { UserSchema } from './user.model.js'
-
-const AdminSchema = UserSchema.clone()
-AdminSchema.path('role').default('ADMIN')
-
-const Admin = mongoose.model('Admin', AdminSchema)
-
-export default Admin
